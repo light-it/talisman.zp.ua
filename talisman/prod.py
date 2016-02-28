2f15a10a5fcbcd39be6943d10455b9d2826a0b9a
kostyachum <kostyachum@gmail.com>
2016-02-28 22:42:25 +0200

    Allowed hosts

--- talisman/prod.py
+++ talisman/prod.py
@@ -4,4 +4,8 @@ import dj_database_url
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+ALLOWED_HOSTS = [
+    'talisman.herokuapp.com',
+    'talisman.zp.ua'
+]
 DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
